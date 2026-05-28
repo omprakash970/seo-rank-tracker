1f48bcb69ec5ce8e5e2da260a3f81f69065b23d7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-28 15:34:13 +0530

    done

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,5 +1,6 @@
-import {createContext, type ReactNode, use, useState} from "react";
+import {createContext, type ReactNode, use, useContext, useState} from "react";
 import type {AxiosInstance} from "axios";
+import axios from "axios";
 import * as process from "node:process";
 interface User{
     id:string,
@@ -28,13 +29,42 @@ export function AppProvider({children}:{children:ReactNode}){
     const[token, setToken]=useStatek<String | null>(null);
     const[loading, setLoading]=useState<boolean>(false);
 
-    const api = axios
+    const api = axios.create({
+
+        baseURL:BACKEND_URL;
+    })
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("token");
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+    const loadUser = async()=>{
+
+    }
+    const login =async ()=>{
+
+    }
+    const register = async ()=>{
+
+    }
+    const logout = async()=>{
+
+    }
+
+    const value = {user, token, loading, api, login, register, logout}
+
 
     const value = {};
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
+}
+export function useApp(){
+    const context = useContext(AppContext);
+    if(!context){
+        throw new Error("useApp must be used within an AppProvider");
 
-
-
+    }
 }
